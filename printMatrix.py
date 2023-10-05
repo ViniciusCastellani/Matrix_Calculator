7774dd18877ba582e8e4f0df69b0a89f69e7f950
ViniciusCastellani <viniciuscastellani07@gmail.com>
2023-10-05 19:09:07 -0300

    Adjust the formating of the printMatrix

--- printMatrix.py
+++ printMatrix.py
@@ -1,7 +1,8 @@
 def printMatrix(matrix1):
     print("\nRESULT")
+    
     for row in matrix1:
-        print("[", end=" ")
+        print("[  ", end=" ")
         for element in row:
-                print(f"{element:5}", end=" ")
-        print("]")
+                print(f"{element:6}", end=" ")
+        print("   ]")
