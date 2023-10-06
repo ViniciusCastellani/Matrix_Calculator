03e52d64ddc0ef61b3c954ae3cdb58cf2145689f
ViniciusCastellani <viniciuscastellani07@gmail.com>
2023-10-05 21:41:16 -0300

    Added new function

--- main.py
+++ main.py
@@ -4,72 +4,117 @@ from printMatrix import printMatrix
 from menu import menu
 from calc import *
 
+
+
 def main():
-    choice = menu()
+    answer = " "
     
-    if choice == '0':
-        return
-    
-    elif choice not in ['6','4','5','7']:
-        print("\nMATRIX 1")
-        matrix1 = initializeMatrix()
+    while True:
         
-        print("\nMATRIX 2")
-        matrix2 = initializeMatrix()
+        if answer == "N":
+            break
         
-    else:
-        matrix1 = initializeMatrix()
-    
-    match choice:
-            case '1':
-                result = add(matrix1, matrix2)
-                if result is not None:
-                    printMatrix(result)
-                else:
-                    print("Matrix addition is not possible.")
-                    
-            case '2':
-                result = sub(matrix1, matrix2)
-                if result is not None:
-                    printMatrix(result)
-                else:
-                    print("Matrix subtraction is not possible.")
-                    
-            case '3':
-                result = mult(matrix1, matrix2)
-                if result is not None:
-                    printMatrix(result)
-                else:
-                    print("Matrix multiplication is not possible.")
+        choice = menu()
+        
+        if choice == '0':
+            return
+        
+        elif choice not in ['6','4','5','7'] and answer != "Y":
+            print("\nMATRIX 1")
+            matrix1 = initializeMatrix()
             
-            case '4':
-                constant = int(input("Insert the constant to multiply the matrix: "))
-                result = multconst(matrix1, constant)
-                if result is not None:
-                    printMatrix(result)
-                else:
-                    print("Invalid constant is not possible.")
+            print("\nMATRIX 2")
+            matrix2 = initializeMatrix()
+        
+        else:
+            matrix1 = initializeMatrix()
+            
+        match choice:
+                case '1':
+                    if answer == "Y":
+                        result = add(result,matrix1)
+                    else:    
+                        result = add(matrix1, matrix2)
+                    
+                    if result is not None:
+                        printMatrix(result)
+                    else:
+                        print("Matrix addition is not possible.")
+                        
+                case '2':
+                    if answer == "Y":
+                        result = sub(result,matrix1)
+                    else:    
+                        result = sub(matrix1, matrix2)
+                    
+                    if result is not None:
+                        printMatrix(result)
+                    else:
+                        print("Matrix subtraction is not possible.")
+                        
+                case '3':
+                    if answer == "Y":
+                        result = mult(result,matrix1)
+                    else:    
+                        result = mult(matrix1, matrix2)
+                    
+                    if result is not None:
+                        printMatrix(result)
+                    else:
+                        print("Matrix multiplication is not possible.")
                 
-            case '5':
-                result = inverse(matrix1)
-                if result is not None:
-                    printMatrix(result)
-                else:
-                    print("Operation not possible.")
-            
-            case '6':
-                result = transpose(matrix1)
-                if result is not None:
-                    printMatrix(result)
-                else:
-                    print("Operation not possible.")
-            
-            case '7':
-                result = determinant(matrix1)
-                if result is not None:
-                    print(f"\nThe determinant of the matrix is = {result:.2f}")
-                else:
-                    print("Operation not possible.")
+                case '4':
+                    if answer == "Y":
+                        constant = int(input("Insert the constant to multiply the matrix: "))
+                        result = multconst(result, constant)
+                    else:    
+                        result = multconst(matrix1, matrix2)
+                    
+                    if result is not None:
+                        printMatrix(result)
+                    else:
+                        print("Invalid constant is not possible.")
+                    
+                case '5':
+                    if answer == "Y":
+                        result = inverse(result)
+                    else:    
+                        result = inverse(matrix1)
+                    
+                    if result is not None:
+                        printMatrix(result)
+                    else:
+                        print("Operation not possible.")
+                
+                case '6':
+                    if answer == "Y":
+                        result = transpose(result)
+                    else:    
+                        result = transpose(matrix1)
+                    
+                    if result is not None:
+                        printMatrix(result)
+                    else:
+                        print("Operation not possible.")
+                
+                case '7':
+                    if answer == "Y":
+                        result = determinant(result)
+                    else:    
+                        result = determinant(matrix1)
+                    
+                    if result is not None:
+                        print(f"\nThe determinant of the matrix is = {result:.2f}")
+                    else:
+                        print("Operation not possible.")
+        
+        while True:
+            answer = input("Do you want to calculate another operation using the result? ").upper()
+            if answer in ["Y","N"]:
+                break
+            else:
+                print("Invalid input")
+    
           
             
 if __name__ == "__main__":
--- printMatrix.py
+++ printMatrix.py
@@ -2,7 +2,7 @@ def printMatrix(matrix1):
     print("\nRESULT")
     
     for row in matrix1:
-        print("[  ", end=" ")
+        print("[ ", end="")
         for element in row:
-                print(f"{element:6}", end=" ")
-        print("   ]")
+                print(f"{element:2}", end=" ")
+        print(" ]")
